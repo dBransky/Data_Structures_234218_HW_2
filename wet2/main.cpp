b75fd9e7c85253241c34b9033da4d093948cbccc
itamararbiv <103099905+itamararbiv@users.noreply.github.com>
2022-06-05 11:36:55 +0300

    Add files via upload

--- wet2/main.cpp
+++ wet2/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "MyHashTable.h"
+#include "MyUnionFind.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    HashTable* hash = new HashTable();
+    UnionFind* UF = new UnionFind(4);
+    UF->Itamar(1, 2);
+    UF->Itamar(2, 1);
+    UF->Itamar(3, 4);
+    UF->Itamar(4, 4);
+    UF->Union(4,3, 0.75);
+    UF->Union(1,4, 0.2);
+    UF->Union(1,2, 1);
+    UF->PrintStatus();
+
     return 0;
 }
